8103740b1aafe2ae2f802ba03f5977157f604ead
1248351274 <yanqingman@126.com>
2020-08-14 00:09:14 +0800

    2020.8.14作业

---
+++ 2020.8.12：数据库/2020-08-12-nodejs-03/代码/mykoa-server/app.js
@@ -0,0 +1,36 @@
+const koa = require('koa');
+
+const koaRouter = require('koa-router');
+
+const mysql = require('mysql2/promise');
+
+let connection;
+~async function(){
+    connection = await mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'root',
+        database:'yanqingTest'
+    })
+}()
+
+// 创建server对象
+const server = new koa();
+
+const router = new koaRouter();
+
+router.get('/',async ctx=>{
+    let username = ctx.query.username;
+    let age = ctx.query.age;
+    console.log(username,age)
+    await connection.execute('insert into `users` (`username`,`age`) values (?,?)',[username,age])
+})
+
+// 把router对象的routes中间件注册到Koa中
+server.use(router.routes())
+
+server.listen(8080,()=>{
+    console.log('服务启动成功,http://locahost:8080')
+})
+
+// http://localhost:8080/?username=xiaoming&age=19
