a6804cb1301e399301b93f16005127e8bc61b3d9
yan <yan@qq.com>
2020-09-10 18:22:22 +0800

    2020.9.10

---
+++ 第三阶段/2020.9.9：鳄鱼，相册vue重构/myphoto/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Button :disabled="disabled" @@clickFn=clickFn()>点击</Button>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button'
+export default {
+  name: 'App',
+  components: {
+    Button
+  },
+  data() {
+    return {
+      disabled: false
+    }
+  },
+  methods: {
+    clickFn() {
+      console.log('dianji');
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
