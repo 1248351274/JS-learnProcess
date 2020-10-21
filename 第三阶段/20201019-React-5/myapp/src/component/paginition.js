acb307aaf9dd43acac3e1918a07bccf8caa8f20d
yanqing <yanqingman@126.com>
2020-10-21 12:24:35 +0800

    react5

--- 第三阶段/20201019-React-5/myapp/src/component/paginition.js
+++ 第三阶段/20201019-React-5/myapp/src/component/paginition.js
@@ -3,7 +3,7 @@ import {Link, useParams} from 'react-router-dom';
 import data from "../data";
 
 function Pagination() {
-  const {type="good",page=1} = useParams();
+  const {type="all",page=1} = useParams();
   let nowData = [];
   if(type == "all"){
     console.log(11);
@@ -15,18 +15,21 @@ function Pagination() {
   } else {
     nowData = data[type];
   }
+  
   const len = 5;
   const pageLen = Math.ceil(nowData.length/len);
   function setNub() {
       let nubs = [];
       for(let i = 1;i<=pageLen;i++){
+        console.log("type",`/list/${type}/${i}`);
         if(i == page){
           nubs.push(<a className="active" key={i}>{i}</a>);
         } else {
-          nubs.push(<Link to={`/${type}/${i}`} key={i}>{i}</Link>);
+          nubs.push(<Link to={`/list/${type}/${i}`} key={i}>{i}</Link>);
         }
        
       }
+      console.log('nubs',nubs);
       return nubs;
   }
   function previousPage(){
@@ -48,9 +51,9 @@ function Pagination() {
     return num
   }
   return (<nav className="pagination">
-      <Link to={`/${type}/${previousPage()}`}>上一页</Link>
+      <Link to={`/list/${type}/${previousPage()}`}>上一页</Link>
       {setNub()}
-      <Link to={`/${type}/${nextPage()}`}>下一页</Link>
+      <Link to={`/list/${type}/${nextPage()}`}>下一页</Link>
     </nav>
     
     )
--- 第三阶段/20201019-React-5/myapp/src/route_list.js
+++ 第三阶段/20201019-React-5/myapp/src/route_list.js
@@ -6,26 +6,20 @@ import ListView from './view/list';
 const types = ["good","share","ask","all"]
 
 const route_list = [
-
   {
-    path: ['/',"/:type","/:type/:page"],
+    path: ['/','/list',"/list/:type","/list/:type/:page"],
     exact:true,
     render(props){
-        const {type="good",page="1"} = props.match.params;
+        const {type="all",page="1"} = props.match.params;
         if(types.includes(type)
         &&page>0
         &&parseInt(page)+"" === page){
           return <ListView {...props} />
         }
-        return <Redirect to="/view404" />
+        return <Redirect to="/404" />
     }
-  },{
-    path: "/view404",
-    render(props){
-      return <View404 {...props} />
-    }
-    
-  },{
+  },
+  {
     path: "",
     render(props){
       return <View404 {...props} />
@@ -36,23 +30,23 @@ const route_list = [
 const list_navs = [
   {
     title: "全部",
-    to: "/all",
+    to: "/list/all",
     exact: false,
     isActive(pathname){
-        return pathname === "/" || pathname.slice(0,4) === "/all"
+        return pathname === "/" || pathname.slice(0,9) === "/list/all"
     }
   },
   {
     title: "精华",
-    to: "/good",
+    to: "/list/good",
     exact: false,
   },{
     title: "分享",
-    to: "/share",
+    to: "/list/share",
     exact: false
   },{
     title: "问答",
-    to: "/ask",
+    to: "/list/ask",
     exact: false
   }
 ];
